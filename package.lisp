152857dafb4768019aa677121b017ece1e9d6468
Alex <alex@pagoda.(none)>
2010-01-25 23:33:06 +0000

    New project twitter initialized by clbuild

---
+++ package.lisp
@@ -0,0 +1,5 @@
+;;; -*- mode: lisp; indent-tabs: nil -*-
+
+(defpackage :twitter
+  (:use :cl)
+  (:export #:test))
---
+++ twitter.lisp
@@ -0,0 +1,6 @@
+;;; -*- mode: lisp; indent-tabs: nil -*-
+
+(in-package :twitter)
+
+(defun test ()
+  (format t "Hello World from new project twitter~%"))
