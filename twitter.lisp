f14153d1e1a84b32f8b4e3db5bde2fe827e98f13
Alex Young <alex@blackkettle.org>
2010-01-28 23:37:44 +0000

    Got the basic queries and posts working.

--- twitter.lisp
+++ twitter.lisp
@@ -3,11 +3,15 @@
   (asdf:oos 'asdf:load-op 'fiveam)
   (asdf:oos 'asdf:load-op 'drakma)
   (asdf:oos 'asdf:load-op 'cl-json))
+(make-requires)
 (defpackage :twitter
   (:use :cl
 	:it.bese.FiveAM
-	:cl-json))
+	:json))
 (in-package :twitter)
+
+(defun get-credentials (username password)
+  (list username password))
 
 (defmacro get-http-string (url &optional credentials)
   `(flexi-streams:octets-to-string
@@ -16,40 +20,84 @@
 			       (list :basic-authorization credentials)
 			       () ))))
 
-(defun get-data-from-json-url (query-url &optional credentials)
-  (json:decode-json-from-string 
-     (get-http-string query-url credentials)))
+(defmacro post-http-string (url body-content &optional credentials)
+  `(flexi-streams:octets-to-string
+    (drakma:http-request ,url
+                         :method :post
+                         ,@(if credentials
+                               (list :basic-authorization credentials)
+                               ())
+                         ,@(if body-content
+                               (list :content body-content)
+                               ()))))
 
-(defun get-timeline (url &optional username password)
-  (get-data-from-json-url url (list username password)))
-(defun get-public-timeline ()
+(defun get-data-from-json-url (credentials query-url)
+  (json:decode-json-from-string 
+   (get-http-string query-url credentials)))
+
+(defun post-data-to-json-url (credentials query-url &optional data-alist)
+  (json:decode-json-from-string
+   (post-http-string query-url (json:encode-json-alist-to-string data-alist) credentials)))
+
+(defun get-timeline (credentials url)
+  (get-data-from-json-url url credentials))
+(defun get-public-timeline (&optional credentials)
   (get-timeline
-   "http://twitter.com/statuses/public_timeline.json")) 
+   "http://twitter.com/statuses/public_timeline.json"
+   credentials)) 
 
 (defvar *private-timeline-urls* 
-  '(("friends" "http://twitter.com/statuses/friends_timeline.json")
-    ("home" "http://api.twitter.com/1/statuses/home_timeline.json")
-    ("user" "http://twitter.com/statuses/user_timeline.json")
-    ("mentions" "http://twitter.com/statuses/mentions.json")
-    ("retweeted-by-me" "http://api.twitter.com/1/statuses/retweeted_by_me.json")))
+  '((friends . "http://twitter.com/statuses/friends_timeline.json")
+    (home . "http://api.twitter.com/1/statuses/home_timeline.json")
+    (user . "http://twitter.com/statuses/user_timeline.json")
+    (mentions . "http://twitter.com/statuses/mentions.json")
+    (retweeted-by-me . "http://api.twitter.com/1/statuses/retweeted_by_me.json")
+    (retweeted-to-me . "http://api.twitter.com/1/statuses/retweeted_to_me.json")
+    (retweets-of-me . "http://api.twitter.com/1/statuses/retweets_of_me.json")))
 
-(loop for (name url) in *private-timeline-urls*
-   do
-     (setf (symbol-function 
-	    (make-symbol (concatenate 'string "get-private-" name "-timeline")))
-	   #'(lambda (un pw) (get-timeline url un pw))))
+(defun get-private-timeline-names ()
+  (mapcar 'car *private-timeline-urls*))
 
-(defun #(intern "add2") (n)
-   (+ n 2))
+(defun get-private-timeline (credentials timeline-name)
+  (let ((timeline-pair (assoc timeline-name *private-timeline-urls*)))
+    (if timeline-pair
+        (get-timeline credentials (cdr timeline-pair))
+        ())))
 
+(defun restful-status-url (&optional op-name status-id)
+  (if status-id
+      (format nil "http://twitter.com/statuses/~(~a~)/~d.json" op-name status-id)
+      "http://twitter.com/statuses/update.json"))
 
-;;; (in-package :twitter.test)
+(defun status-get (credentials status-id)
+  (get-data-from-json-url credentials (restful-status-url 'show status-id)))
 
-(test get-xmls
-  (is (equal "a" (xmls-tag (get-xmls "<a/>"))))
-  (is (equal '(("b" "c")) (xmls-attrs (get-xmls "<a b='c'/>"))))
-  (is (equal "b" (xmls-tag (first (xmls-children (get-xmls "<a><b/></a>")))))))
+;;; This function builds the alist which will eventually be
+;;; json-encoded and posted. It leaves NILs lying around
+;;; for missing values, but that doesn't matter - they get stripped
+;;; out by json:encode-json-alist-to-string.
+(defun build-post-status-alist (status in-reply-to-id lat long)
+  `(("status" . ,status)
+    ,(if in-reply-to-id `("in_reply_to_status_id" . ,in-reply-to-id))
+    ,(if lat `("lat" . ,lat))
+    ,(if long `("long" . ,long))))
 
+(defun status-post (credentials status &key in-reply-to-id lat long)
+  (post-data-to-json-url credentials
+                         (restful-status-url)
+                         (build-post-status-alist (status in-reply-to-id lat long))))
+(defun status-delete (credentials status-id)
+  (post-data-to-json-url credentials
+                         (restful-status-url 'destroy status-id)))
 
+(defun status-retweet (credentials status-id)
+  (post-data-to-json-url credentials
+                         (restful-status-url 'retweet status-id)))
 
+(defun retweets-url (status-id count)
+  (format nil
+          "http://api.twitter.com/1/statuses/retweets/~d.json~[~:;&count=~d~]"
+          status-id (if (numberp count) count 0) count))
 
+(defun status-retweets (credentials status-id &optional count)
+  (get-data-from-json-url credentials (retweets-url status-id count)))
