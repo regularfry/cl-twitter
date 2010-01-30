49d4b4c9b71da8e1ab31d2ebd799a478297404ac
Alex Young <alex@blackkettle.org>
2010-01-30 01:00:22 +0000

    Added search and apply

--- twitter.lisp
+++ twitter.lisp
@@ -5,18 +5,29 @@
   (asdf:oos 'asdf:load-op 'cl-json))
 (make-requires)
 (defpackage :twitter
-  (:use :cl))
+  (:use :cl)
+  (:export :search
+           :trends
+           :trends-current
+           :trends-daily
+           :trends-weekly
+           ))
 (in-package :twitter)
+
+;;; For my reference rather than anything else right now
+(defvar *exports*)
 
 (defun get-credentials (username password)
   (list username password))
 
-(defmacro get-http-string (url &optional credentials)
+(defmacro get-http-string (url &optional credentials query-data)
   `(flexi-streams:octets-to-string
     (drakma:http-request ,url 
 			 ,@(if credentials 
 			       (list :basic-authorization credentials)
-			       () ))))
+			       () )
+                         ,@(if query-data
+                               (list :parameters query-data)) )))
 
 (defmacro post-http-data (url query-data &optional credentials)
   `(flexi-streams:octets-to-string
@@ -29,9 +40,9 @@
                                (list :parameters query-data)
                                ()))))
 
-(defun get-data-from-json-url (credentials query-url)
+(defun get-data-from-json-url (credentials query-url &optional query-data)
   (json:decode-json-from-string 
-   (get-http-string query-url credentials)))
+   (get-http-string query-url credentials query-data)))
 
 (defun post-data-to-json-url (credentials query-url &optional data-alist)
   (json:decode-json-from-string
@@ -100,3 +111,56 @@
 
 (defun status-retweets (credentials status-id &optional count)
   (get-data-from-json-url credentials (retweets-url status-id count)))
+
+
+
+
+
+
+;;; Search functions
+
+;;; We want to match the URL name with the function name, which means
+;;; that we need to shadow CL:SEARCH. 
+(let ((scope (multiple-value-bind (_ s) (find-symbol "SEARCH" :twitter) s)))
+  ;; If we're going to have problems, the symbol scope will be :INHERITED.
+  (if (eq scope :INHERITED) (shadow "SEARCH" :twitter)))
+
+;;; We can be called with all the usual twitter search parameters
+;;; (except callback), and we return a list of alists.
+(defun search (query &key lang locale results-per-page page since-id geocode-lat geocode-long geocode-radius show-user)
+  (get-data-from-json-url ()
+                          "http://search.twitter.com/search.json"
+                          `(("q" . ,query)
+                            ;; Again, rely on json:encode-blah to
+                            ;; get rid of nils for us
+                            ,(if lang `("lang" . ,lang))
+                            ,(if locale `("locale" . ,locale))
+                            ,(if results-per-page `("rpp" . ,results-per-page))
+                            ,(if page `("page" . ,page))
+                            ,(if since-id `("since_id" . ,since-id))
+                            ,(if geocode-lat `("geocode" . ,(format nil "~a,~a,~a" geocode-lat geocode-long geocode-radius)))
+                            ,(if show-user '("show_user" . "true")))))
+
+(defun trends-url (&optional period)
+  (if period
+      (format nil "http://search.twitter.com/trends/~(~a~).json" period)
+      "http://search.twitter.com/trends.json"))
+
+(defun trends ()
+  (get-data-from-json-url () (trends-url)))
+
+(defun trends-current (&optional exclude-hashtags?)
+  (get-data-from-json-url ()
+                          (trends-url 'current)
+                          (if exclude-hashtags? '(("exclude" . "hashtags")))))
+;;; If given, date should be YYYY-MM-DD
+(defun trends-daily (&optional date exclude-hashtags?)
+  (get-data-from-json-url ()
+                          (trends-url 'daily)
+                          `(,(if date `("date" . ,date))
+                             ,(if exclude-hashtags? '("exclude" . "hashtags")))))
+(defun trends-weekly (&optional date exclude-hashtags?)
+  (get-data-from-json-url ()
+                          (trends-url 'weekly)
+                          `(,(if date `("date" . ,date))
+                             ,(if exclude-hashtags? '("exclude" . "hashtags")))))
